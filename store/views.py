fd5ad9ea12b555585026ca91f51a1dd97e87fa44
appleSheep <a14506819@gmail.com>
2024-10-31 22:49:42 +0800

    add login logout function

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product, Category
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     products = Product.objects.all()
@@ -8,3 +10,23 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, ("Successfully Login !"))
+            return redirect('home')
+        else:
+            messages.error(request, ("Login Error !"))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("Successfully Logout !"))
+    return redirect('home')
