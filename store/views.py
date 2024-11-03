288325f01a4f48839e5f749d3341e58f2b725b5b
appleSheep <a14506819@gmail.com>
2024-11-03 16:42:57 +0800

    register page

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 def home(request):
     products = Product.objects.all()
@@ -11,18 +15,21 @@ def home(request):
 def about(request):
     return render(request, 'about.html', {})
 
+def run_login(request, username, password):
+    user = authenticate(request, username=username, password=password)
+    if user:
+        login(request, user)
+        messages.success(request, ("Successfully Login !"))
+        return redirect('home')
+    else:
+        messages.error(request, ("Login Error !"))
+        return redirect('login')
+
 def login_user(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user:
-            login(request, user)
-            messages.success(request, ("Successfully Login !"))
-            return redirect('home')
-        else:
-            messages.error(request, ("Login Error !"))
-            return redirect('login')
+        return run_login(request, username, password)
     else:
         return render(request, 'login.html', {})
 
@@ -30,3 +37,18 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("Successfully Logout !"))
     return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # login user
+            return run_login(request, username, password)
+        else:
+            messages.error(request, ("Register Error !"))
+            return redirect('register')
+    return render(request, 'register.html', {'form': form})
